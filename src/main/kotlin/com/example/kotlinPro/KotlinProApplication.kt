69a8c4d67b17edae450c6f2a2050b8cd5d20e7a9
TaeHyun77 <acd2283@gmail.com>
2025-04-28 23:50:30 +0900

    feat first commit

---
+++ src/main/kotlin/com/example/kotlinPro/KotlinProApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinPro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinProApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinProApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinPro/KotlinProApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinPro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinProApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
